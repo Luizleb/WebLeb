ef1d5d6a47d3e7116de2a224aa98b90a47da777b
Leb <luizleb@gmail.com>
2020-04-21 15:28:12 -0300

    Using a simple database

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/plain")
-	fmt.Fprint(w, "<h2>Agora utilizando HandleFunc</h2>")
+var db map[string]float32
+
+func list(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "text/html")
+	for k, val := range db {
+		fmt.Fprintf(w, "<h3>Item:  %s, valor : %v</h3>\n", k, val)
+	}
 }
 
-func teste(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("content-type", "text/html")
-	fmt.Fprint(w, "<h2>Página de Teste</h2>")
+func price(w http.ResponseWriter, r *http.Request) {
+	reqItem := r.URL.Query().Get("item")
+	price, ok := db[reqItem]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "No such item: %q\n", reqItem)
+		return
+	}
+	fmt.Fprintf(w, "Price for item : %s = %v\n", reqItem, price)
 }
 
 func main() {
-	http.HandleFunc("/home", home)
-	http.HandleFunc("/teste/", teste)
+	db = map[string]float32{
+		"openGL": 100,
+		"Golang": 65,
+	}
+	http.HandleFunc("/list", list)
+	http.HandleFunc("/price", price)
 	http.ListenAndServe(":3000", nil)
 }
